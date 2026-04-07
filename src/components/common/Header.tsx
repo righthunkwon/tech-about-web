4f14af6a7c14b4253249c66fd1a4bd5078c48ff8
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2026-04-08 00:22:44 +0900

    design: 다크모드 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '@/assets/svgs/logo.svg';
 import HeaderDrawerMenu from '@/components/common/HeaderDrawerMenu';
@@ -8,17 +8,17 @@ const Header: React.FC = () => {
   const [open, setOpen] = useState(false); // 메뉴 오픈 여부
   const location = useLocation(); // 현재 라우터의 위치
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setOpen(false);
   }, [location.pathname]);
 
   return (
     <>
-      <header className="bg-ta-base fixed top-0 z-100 mx-auto flex h-15 w-full justify-center border-b border-gray-200">
+      <header className="bg-ta-base border-ta-divider fixed top-0 z-100 mx-auto flex h-15 w-full justify-center border-b">
         <nav className="ta-content flex justify-between px-4">
           <Link to="/" className="flex items-center gap-1 px-2">
             <img className="h-6 w-6" src={logo} alt="logo" />
-            <span className="text-ta-black text-md font-semibold">
+            <span className="text-ta-base-title text-md font-semibold">
               tech-about
             </span>
           </Link>
