bc5bcf4f5c77ad91de1c3046ef12d89c07144473
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2026-04-09 21:39:45 +0900

    feat: 타이머 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Layout from '@/components/layout/Layout';
 import HomePage from '@/domains/home/HomePage';
 import NotFoundPage from '@/domains/not-found/NotFoundPage';
 import SamplePage from '@/domains/admin/SamplePage';
+import TimerPage from '@/domains/timer/TimerPage';
 import Loading from '@/components/common/Loading';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/sample" element={<SamplePage />} />
+            <Route path="/timer" element={<TimerPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
